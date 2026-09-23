d3cde21cde14158dab7a4af58cfeecb97f3134f8
Sakib Hossen Shehan <sakibshehan2@gmail.com>
2026-09-23 11:43:13 +0600

    Added unmarshal Structure

--- controllers/property.go
+++ controllers/property.go
@@ -1,1 +1,13 @@
 package controllers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+type PropertyController struct {
+	beego.Controller
+}
+
+func (c *PropertyController) Get() {
+	c.Ctx.WriteString("Hello Worlds")
+}
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,3 @@
-// @APIVersion 1.0.0
-// @Title beego Test API
-// @Description beego has a very cool tools to autogenerate documents for your API
-// @Contact astaxie@gmail.com
-// @TermsOfServiceUrl http://beego.me/
-// @License Apache 2.0
-// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
 package routers
 
 import (
@@ -16,15 +9,9 @@ import (
 func init() {
 	ns := beego.NewNamespace("/v1",
 		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
+			beego.NSRouter("/", &controllers.PropertyController{}),
 		),
 	)
+
 	beego.AddNamespace(ns)
 }
